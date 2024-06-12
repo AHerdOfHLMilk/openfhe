00210ecc549f0e5dd387e9ee6d892844b84c7fbc
HLMilk <tanhl2000@gmail.com>
2024-06-12 19:03:32 +0800

    Added poly-eval

---
+++ build/CMakeFiles/prefix-sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prefix-sum.dir/prefix-sum.cpp.o"
+  "prefix-sum"
+  "prefix-sum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prefix-sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
